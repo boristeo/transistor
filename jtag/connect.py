6d11e09356a95c92ae035fa22beaec1e559bab2e
Boris Teodorovich <boristeodorovich@gmail.com>
2019-10-06 14:59:54 -0700

    Some more refactoring

--- jtag/connect.py
+++ jtag/connect.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python3
 from pyftdi import jtag
 from pyftdi.ftdi import Ftdi
-from pyftdi.bits import BitSequence as bs
+from pyftdi.bits import BitSequence
+bs = BitSequence
 from binascii import hexlify
 from array import array
 
 ### CONSTANTS
 
-TO_IDLE = bs('111110')
+RESET_TO_IDLE = bs('111110')
 IDLE_TO_SHIR = bs('1100')
 IDLE_TO_SHDR = bs('100')
+SHIR_TO_IDLE = bs('110')
+SHDR_TO_IDLE = bs('110')
+SHDR_TO_SHIR = bs('111100')
+SHIR_TO_SHDR = bs('11100')
 
 
-# instruction register is actually 10 bits, but that doesn't seem to matter
-I_EXTEST    = bs('00000')
-I_SAMPLE    = bs('00001')
-I_USER1     = bs('00010')
-I_USER2     = bs('00011')
+# The example uses 6 bit instructions, with TAP 1 being shifted on the MSB
+# speaking of MSB, the instructions are supposed to be shifted from LSB, hence the reverse
+I_EXTEST    = bs('000000').reverse()
+I_SAMPLE    = bs('000001').reverse()
+I_USER1     = bs('000010').reverse()
+I_USER2     = bs('000011').reverse()
 I_CFG_OUT   = bs('000100').reverse()
 I_CFG_IN    = bs('000101').reverse()
-I_INTEST    = bs('00111')
-I_INTEST    = bs('01000')
-I_USERCODE  = bs('01000')
-I_IDCODE    = bs('01001')
-I_HIGHZ     = bs('01010')
-I_JSTART    = bs('01100')
-I_BYPASS    = bs('11111')
+I_INTEST    = bs('000111').reverse()
+I_INTEST    = bs('001000').reverse()
+I_USERCODE  = bs('001000').reverse()
+I_IDCODE    = bs('001001').reverse()
+I_HIGHZ     = bs('001010').reverse()
+I_JSTART    = bs('001100').reverse()
+I_BYPASS    = bs('011111').reverse()
 
 R_CRC       = bs('00000')
 R_FAR       = bs('00001')
@@ -73,7 +79,7 @@ W_NOOP      = b'\x20\x00\x00\x00'
 W_DUMMY     = b'\xff\xff\xff\xff'
 W_readSTAT  = b'\x28\x00\xe0\x01'
 
-### Adding utility functions to pyftdi
+### Patching pyftdi so it will be less stupid
 def configure(self, url):
   self._ftdi.open_mpsse_from_url(url, direction=self.direction, frequency=self._frequency)
   self._ftdi.write_data(bytearray(b'\x8a\x97\x8d'))
@@ -82,36 +88,58 @@ def configure(self, url):
   self._ftdi.write_data(bytearray(b'\x82\x00\x00'))
 setattr(jtag.JtagController, 'configure', configure)
 
-def _write_bytes_raw(self, out, msb=False):
+def _write_bytes(self, out, msb=False):
   olen = len(out)-1
   order = Ftdi.WRITE_BYTES_NVE_MSB if msb else Ftdi.WRITE_BYTES_NVE_LSB
   cmd = array('B', (order, olen & 0xff, (olen >> 8) & 0xff))
   cmd.extend(out)
   self._stack_cmd(cmd)
-setattr(jtag.JtagController, '_write_bytes_raw', _write_bytes_raw)
+setattr(jtag.JtagController, '_write_bytes', _write_bytes)
 
-def rread(self, reg, bits=64):
-  self.write_tms(TO_IDLE)
+def write(self, out, use_last=True, msb=True):
+  if isinstance(out, bytes):
+    if not use_last:
+      self._write_bytes(out, msb=msb)
+      return
+    if len(out) > 1:
+      self._write_bytes(out[:-1], msb=msb)
+    out = BitSequence(value=out[-1])
+    if msb:
+      out.reverse()
+  elif not isinstance(out, BitSequence):
+    out = BitSequence(out)
+  if use_last:
+    (out, self._last) = (out[:-1], bool(out[-1]))
+  byte_count = len(out)//8
+  if byte_count > 0 and not msb:
+    raise Exception('ONLY MSB SUPPORTED FOR BITS')
+  pos = 8*byte_count
+  bit_count = len(out)-pos
+  if byte_count:
+    self._write_bytes(out[:pos].tobytes(msby=True))
+  if bit_count:
+    self._write_bits(out[pos:])
+setattr(jtag.JtagController, 'write', write)
+
+def readidcode(self):
+  self.write_tms(RESET_TO_IDLE)
   self.write_tms(IDLE_TO_SHDR)
-  return hexlify(bytearray(self.shift_register(bs('0' * bits)).tobytes()))
-setattr(jtag.JtagController, 'rread', rread)
+  return hexlify(bytearray(self.shift_register(bs('0' * 64)).tobytes()))
+setattr(jtag.JtagController, 'readidcode', readidcode)
 
 def readstat(self):
-  self.write_tms(TO_IDLE)
+  self.write_tms(RESET_TO_IDLE)
   self.write_tms(IDLE_TO_SHIR)
   self.write(I_CFG_IN)
-  
-  self.write_tms(bs('11100'))
-  data = W_SYNC + W_NOOP + W_readSTAT + W_NOOP + W_NOOP
-  self._write_bytes_raw(data[:-1], msb=True)
-  self.write(bs(bin(data[-1])))
-
-  self.write_tms(bs('111100'))
+  self.write_tms(SHIR_TO_SHDR)
+  data = W_SYNC + W_NOOP + W_readSTAT + W_DUMMY + W_DUMMY
+  self.write(data, msb=True)
+  self.write_tms(SHDR_TO_SHIR)
   self.write(I_CFG_OUT)
-  self.write_tms(bs('11100'))
-
+  self.write_tms(SHIR_TO_SHDR)
   return hexlify(bytearray(self.shift_register(bs('0' * 32)).tobytes()))
 setattr(jtag.JtagController, 'readstat', readstat)
+
 
 def cwrite(self, cmd):
   self.write_tms(TO_IDLE)
@@ -138,5 +166,5 @@ url = 'ftdi://0x403:0x6010/1'
 j.configure(url)
 
 # Setup env
-print(j.rread(R_IDCODE))
-
+print('ID:  ', j.readidcode())
+print('STAT:', j.readstat())
